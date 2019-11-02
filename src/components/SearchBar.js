a0bc54f40dd9fed2221dcb766f7f757d05629992
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-11-02 21:28:21 +0200

    It gets the search word from the search screen and ready for adding code to make a request to YELP API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange, onTermSubmit}) => { //destructuring props object, taking two 
+    // necessary props from and using them for managing TextInput.
   return (
-  <View style={styles.background}>
-    <Text>Search Bar</Text>  
+  <View style={styles.backgroundStyle}>
+    <Feather name='search' style={styles.iconStyle} />
+    <TextInput
+      autoCapitalize='none' //turn off automatic capitalization
+      autoCorrect={false} //turn off auto correction
+      style={styles.inputStyle} 
+      placeholder='Search'
+      value={term}
+      onChangeText={(newTerm) => onTermChange(newTerm)} 
+      //Could be shortened to onChangeText={onTermChange}
+      //Now the InputText is going to be told-> value={term} what the exact value of  
+      //it should be. And any time a user changes that text with the onChangeText callback 
+      //we're going to call onTermChange wich is passed from SearchScreen parent component 
+      //and we'll pass in the newTerm(the new string) that the user just entered.
+      onEndEditing={onTermSubmit} //the shortened from onEndEditing={() => onTermSubmit()}
+    />
   </View>  
   )};
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: '#F0EEEE',
     height: 50,
     borderRadius: 5,
-    margin: 10
-  }  
+    marginTop: 10,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18 //increase font size in the Input Field
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 10
+  }
 });
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const[term, setTerm] = useState(''); //piece of state, managed by SearchScreen;
+
   return <View>
-    <SearchBar />
-    <Text>Search Screen</Text>  
+    <SearchBar 
+      term={term} 
+      onTermChange={(newTerm) => {setTerm(newTerm)} }
+      //passing down that piece of state -> term={term} and a way -> onTermChange={(newTerm) => {setTerm(newTerm)}
+      //to change it down to the child SearchBar element; then we need to show up these 
+      //to properties (term and onTermChange) inside of our props object -> see SearchBar
+      onTermSubmit={() => console.log(term)}
+    /> 
+    <Text>Search Screen</Text> 
+    <Text>{term}</Text> 
   </View>  
 };
 
 const styles = StyleSheet.create({});
 
 export default SearchScreen;
+
+
+//term - means expression;
