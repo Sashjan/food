babc3c7abc48b1408f868b0f75958732148ba687
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-11-07 09:46:25 +0200

    added state for api and added api request

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,35 @@
 import React, {useState} from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp'
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const[term, setTerm] = useState(''); //piece of state, managed by SearchScreen;
+  const[business, setBusiness] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term, //because of term: term we use ES2015 syntax and just write down single term  
+        location: 'san jose'
+      }  
+    });
+    setBusiness(response.data.businesses);
+  };
 
   return <View>
     <SearchBar 
       term={term} 
-      onTermChange={(newTerm) => {setTerm(newTerm)} }
-      //passing down that piece of state -> term={term} and a way -> onTermChange={(newTerm) => {setTerm(newTerm)}
+      onTermChange={setTerm}
+      //passing down that piece of state -> term={term} and a way -> onTermChange={(newTerm) => {setTerm(newTerm)} 
+      //or we can short it to just onTermChange={setTerm}
       //to change it down to the child SearchBar element; then we need to show up these 
       //to properties (term and onTermChange) inside of our props object -> see SearchBar
-      onTermSubmit={() => console.log(term)}
+      onTermSubmit={searchApi}
     /> 
     <Text>Search Screen</Text> 
-    <Text>{term}</Text> 
+    <Text>We have found {business.length} results</Text> 
   </View>  
 };
 
