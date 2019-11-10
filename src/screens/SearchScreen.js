fa603cb568ab350f070568350c3e5a2b754cd250
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-11-10 09:15:12 +0200

    added useEffect to make a request when SearchScreen first rendered

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
@@ -8,12 +8,13 @@ const SearchScreen = () => {
   const[business, setBusiness] = useState([]);
   const[errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
+    console.log('Hi, There!');
     try{
       const response = await yelp.get('/search', {
           params: {
           limit: 50,
-          term, //because of term: term we use ES2015 syntax and just write down single term  
+          term: searchTerm, //because of term: term we use ES2015 syntax and just write down single term  
           location: 'san jose'
           }  
       });
@@ -22,11 +23,20 @@ const SearchScreen = () => {
         setErrorMessage('Something went wrong');
     }
   };
+  //searchApi('pasta');
+// Call searchApi when component is first rendered.
+//This is BAD CODE! Don't do that because it creates 
+//an infinite loop(infinite screen rendering, i.e UPDATED STATE 
+//CAUSES COMPONENT TO RERENDER) meaning it calls SearchScreen function again and again.
+useEffect(() => {
+  searchApi('pasta');
+}, []);
 
+//The block of code below is called JSX block.
   return <View>
     <SearchBar 
       term={term} 
-      onTermChange={setTerm}
+      onTermChange={() => setTerm(term)}
       //passing down that piece of state -> term={term} and a way -> onTermChange={(newTerm) => {setTerm(newTerm)} 
       //or we can short it to just onTermChange={setTerm}
       //to change it down to the child SearchBar element; then we need to show up these 
