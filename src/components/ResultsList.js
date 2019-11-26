3ec7c34ff6540d4a15313664ab7c8e1f88e99283
Alexander Korotnyan <alexander.korotnyan@gmail.com>
2019-11-26 21:07:05 +0200

    styling the list of the results on the SearchScreen

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
-import useRestaurants from '../hooks/useRestaurants';
+import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({title, results}) => {
+  console.log(results);
 
-
-  return <View>
+  return <View style={styles.container}>
     <Text style={styles.title}>{title}</Text>
     <FlatList
+      showsHorizontalScrollIndicator={false}
       horizontal //equivalent to the row of 'horizontal={true}'.
       data={results}
       keyExtractor={(result) => result.id}
       renderItem={({item}) => {
-        return <Text>{item.name}</Text>;
+        return <ResultsDetail result={item} />;
       }}
     />
     </View>
@@ -21,7 +22,12 @@ const ResultsList = ({title, results}) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 8
   }
 });
 
